4c370e9d03b41a8a7a4c9af4343bea22da434e39
Nikita <torchhik_darkness@mail.ru>
2023-07-12 14:34:28 +0300

    Update project

---
+++ frontend/src/components/UI/XButton.vue
@@ -0,0 +1,24 @@
+<template>
+  <button class="x-btn" v-on="$listeners">
+    <slot></slot>
+  </button>
+</template>
+  
+<script>
+export default {
+  name: "XButton"
+}
+</script>
+  
+<style scoped>
+.x-btn {
+  border: 0.5px solid #0738fd;
+  background-color: #0738fd;
+  color: white;
+  padding: 5px 10px;
+  font-size: 14px;
+  cursor: pointer;
+  border-radius: 10px;
+  box-shadow: 0px 5px 10px rgba(0, 0, 0, 0.1);
+}
+</style>
---
+++ frontend/src/components/UI/index.js
@@ -0,0 +1,11 @@
+import XButton from "@/components/UI/XButton.vue";
+import XInput from "@/components/UI/XInput.vue";
+// import XDialog from "@/components/UI/XDialog.vue";
+// import XSelect from "@/components/UI/XSelect.vue";
+
+export default [
+    XButton, // Компонент кнопки
+    XInput,
+    // XDialog,
+    // XSelect
+]

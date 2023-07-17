3a331b381e8778dffc82b430a200c25ef3165a12
Nikita <torchhik_darkness@mail.ru>
2023-07-17 21:16:26 +0300

    full edit

---
+++ frontend/src/pages/HomePage.vue
@@ -0,0 +1,15 @@
+<script>
+import {defineComponent} from 'vue'
+
+export default defineComponent({
+name: "HomePage"
+})
+</script>
+
+<template>
+  $END$
+</template>
+
+<style scoped>
+
+</style>
---
+++ frontend/src/pages/LoginPage.vue
@@ -0,0 +1,15 @@
+<script>
+import {defineComponent} from 'vue'
+
+export default defineComponent({
+name: "LoginPage"
+})
+</script>
+
+<template>
+  $END$
+</template>
+
+<style scoped>
+
+</style>

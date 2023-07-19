7444663c701caf6d6a816c984fec30952613c8d9
Nikita <torchhik_darkness@mail.ru>
2023-07-19 14:26:23 +0300

    build production

--- backend/library/settings.py
+++ backend/library/settings.py
@@ -74,7 +74,7 @@ ROOT_URLCONF = 'library.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': [ BASE_DIR / "frontend/dist" ],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
